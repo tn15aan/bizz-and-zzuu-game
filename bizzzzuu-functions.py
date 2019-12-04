1a0510f793377cb91a27f4c3c721a6b6456d69c6
Thomas Nguyen <TNguyen@spartaglobal.com>
2019-12-04 15:54:58 +0000

    Seperated and added new files for tests and functions

---
+++ bizzzzuu-functions.py
@@ -0,0 +1,9 @@
+def bizz_zzuu(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return str(num) + ' is a multiple of 3 or 5, bizzzzuu'
+    elif num % 3 == 0:
+        return str(num) + ' is a multiple of 3, bizz!'
+    elif num % 5 == 0:
+        return str(num) + ' is a multiple of 5, zzuu!'
+    else:
+        return str(num) + ' is not a multiple of 3 or 5, please try again.'
---
+++ bizzzzuu-tests.py
@@ -0,0 +1,5 @@
+from bizzzzuu-functions import *
+
+print(bizz_zzuu(30))
+print(bizz_zzuu(45))
+print(bizz_zzuu(7))
